c11343ce49038c1101a9581a11ea258402155b81
3ll3d00d <mattkhan@gmail.com>
2015-02-11 00:36:41 +0000

    expanded cmdline options

--- generate.sh
+++ generate.sh
@@ -1,32 +1,142 @@
-#/bin/bash
+#/bin/bash -x 
 
 # data should be exported as space delimited freq/dB (usually has phase as 3rd column)
-# filenames should be <degrees>.dat
+# filenames should be <foo>_<degrees>.dat where foo is some consistent prefix and degrees can be 90 or -90
+FORCE_DELETE=0
+MIN_FREQ=200
+MAX_FREQ=24000
+AUTO_MIRROR=0
+TARGET_DIR="$(pwd)"
+WIDTH=1920
+HEIGHT=1080
+PREFIX=
 
-rm directivity.txt
-rm sorted_directivity.txt
-rm gnuplot_input.txt
+function usage {
+    echo "generate.sh -d -l 100 -h 22000 -m -x 1920 -y 1080 -p foo"
+    echo "    -d force delete of existing generated files"
+    echo "    -l sets the lo frequency for the data generated, if unset default to the minimum value in the input data (NB: actually 200 for now)"
+    echo "    -h sets the hi frequency for the data generated, if unset default to the maximum value in the input data (NB: actually 24000 for now)"
+    echo "    -m mirror mode"
+    echo "    -x width of image in pixels, default 1920"
+    echo "    -y height of image in pixels, default 1080"
+    echo "    -p file name prefix"
+}
 
-# TODO accept limits at cmdline
-MIN_FREQ=800
+function delete_or_blow {
+    if [ "${FORCE_DELETE}" -eq 1 ]
+    then
+	[[ -f "${1}" ]] && rm "${1}"
+    else
+        [[ -f "${1}" ]] && echo "${1} exists, exiting! use -d to continue or move the file" && exit 65
+    fi    
+}
 
-echo "Generating data for gnuplot from ${MIN_FREQ} Hz"
+function mirror_data {
+    for each in $(ls ${PREFIX}_[0-9]*.dat)
+    do
+        local ROOT="${each%%.dat}"
+        local DEGREES="${ROOT##*_}"
+        if [ "${DEGREES}" != 0 ]
+        then
+            [[ ! -e "${PREFIX}_-${DEGREES}.dat" ]] && cp "${each}" "${PREFIX}_-${DEGREES}.dat"
+        fi
+    done
+    for each in $(ls ${PREFIX}_-[0-9]*.dat)
+    do
+        local ROOT="${each%%.dat}"
+	local DEGREES="${ROOT##*_}"
+	local DEGREES="${DEGREES:1}"
+	[[ ! -e "${PREFIX}_${DEGREES}.dat" ]] && cp "${each}" "${PREFIX}_${DEGREES}.dat"
+    done
+}
+
+while getopts "mdl:h:x:y:p:" OPTION
+do
+     case $OPTION in
+         m)
+             SHIFT_COUNT=$((SHIFT_COUNT+1))
+             AUTO_MIRROR=1
+             ;;
+	 d)
+	     SHIFT_COUNT=$((SHIFT_COUNT+1))
+	     FORCE_DELETE=1
+	     ;;
+	 l)
+	     SHIFT_COUNT=$((SHIFT_COUNT+2))
+	     MIN_FREQ="${OPTARG}"
+	     ;;
+	 h)
+	     SHIFT_COUNT=$((SHIFT_COUNT+2))
+	     MAX_FREQ="${OPTARG}"
+	     ;;
+	 x)
+	     SHIFT_COUNT=$((SHIFT_COUNT+2))
+	     WIDTH="${OPTARG}"
+	     ;;
+	 y)
+	     SHIFT_COUNT=$((SHIFT_COUNT+2))
+	     HEIGHT="${OPTARG}"
+	     ;;
+	 p)
+	     SHIFT_COUNT=$((SHIFT_COUNT+2))
+	     PREFIX="${OPTARG}"
+	     ;;
+         ?)
+             usage
+             exit 1
+             ;;
+     esac
+done
+shift ${SHIFT_COUNT}
+
+if [ $# -eq 1 ]
+then
+    TARGET_DIR="${1}"    
+fi
+if [ -z "${PREFIX}" ]
+then
+    echo "Measurements must have a prefix"
+    exit 67
+fi
+
+echo "Generating data from ${TARGET_DIR} with prefix ${PREFIX} in range ${MIN_FREQ} Hz to ${MAX_FREQ} Hz"
+
+delete_or_blow "${PREFIX}_directivity.txt"
+delete_or_blow "${PREFIX}_sorted_directivity.txt"
+delete_or_blow "${PREFIX}_gnuplot.txt"
+
+cd "${TARGET_DIR}"
+
+if [ ${AUTO_MIRROR} -eq 1 ]
+then
+    mirror_data
+fi
+
+# parse the dat files into a single directivity file
 for each in $(ls *.dat)
 do 
-    DEGREES=$(echo ${each%%.dat})
-    echo "Parsing ${each}"
-    awk -F" " -v deg=${DEGREES} -v minfreq=${MIN_FREQ} '/^[0-9]/ { if ( $1 > minfreq ) { print $1, deg, $2 } }' $each >> directivity.txt
+    ROOT="${each%%.dat}"
+    DEGREES="${ROOT##*_}"
+    echo "Parsing ${DEGREES} from ${each}"
+    awk -F" " -v deg=${DEGREES} -v minfreq=${MIN_FREQ} -v maxfreq=${MAX_FREQ} '/^[0-9]/ { if ( $1 >= minfreq ) { if ( $1 <= maxfreq ) { print $1, deg, $2 } } }' $each >> directivity.txt
 done
+[[ ! -e directivity.txt ]] && echo "No files processed" && exit 66
+DATA_ROWS="$(wc -l < directivity.txt)"
+[[ ${DATA_ROWS} -eq 0 ]] && echo "No data found" && exit 67
+# sort by frequency and degrees
 sort -k1V,2V directivity.txt > sorted_directivity.txt
-awk -v n=13 '1; NR % n == 0 {print ""}' sorted_directivity.txt > gnuplot_input.txt
+# add a blank row after every batch of frequency data
+# NB: assumes each input file is identical, bad things will happen otherwise!!
+NO_OF_AXIS_MEASUREMENTS="$(cut -d" " -f2 directivity.txt |sort |uniq|wc -l)"
+awk -v n=${NO_OF_AXIS_MEASUREMENTS} '1; NR % n == 0 {print ""}' sorted_directivity.txt > gnuplot_input.txt
 
-echo "Plotting!"
+echo "Plotting output.png (size ${WIDTH} x ${HEIGHT})"
 
 # TODO work out min/max levels from input data + pass through to gnuplot
 
 gnuplot <<EOF
 # set the input and output
-set term png size 1728,972 crop
+set term png size ${WIDTH},${HEIGHT} crop
 set output "output.png"
 set datafile separator " "
 
@@ -40,12 +150,13 @@ set pm3d interpolate 20,20
 
 # formatting of axes etc
 set logscale x 10
-set xrange [800:22050]
+set xrange [${MIN_FREQ}:${MAX_FREQ}]
 set mxtics 10
 set xlabel 'Frequency (Hz)'
 
 set ylabel 'deg'
-set ytics 0,7.5,90
+set yrange [-90:90]
+set ytics -90,7.5,90
 
 set key outside
 set key top right
